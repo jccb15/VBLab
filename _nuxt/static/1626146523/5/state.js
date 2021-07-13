18ab702bda7f2efa5519840c543795394b1c86b3
Jose <jose.castelo@pusan.ac.kr>
2021-07-13 12:22:06 +0900

    Updates

---
+++ _nuxt/static/1626146523/5/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FVBLab\u002F_nuxt\u002Fstatic\u002F1626146523",layout:"default",error:a,serverRendered:true,routePath:"\u002F5",config:{_app:{basePath:"\u002FVBLab\u002F",assetsPath:"\u002FVBLab\u002F_nuxt\u002F",cdnURL:a}}}}(null));
