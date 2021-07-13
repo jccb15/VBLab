627a095cdf9330eda8954dd96e81b47d29d6eeee
Jose <jose.castelo@pusan.ac.kr>
2021-07-13 16:25:56 +0900

    Updates

---
+++ _nuxt/db613fe.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{375:function(n,e,t){"use strict";t.r(e);var r={},c=t(78),component=Object(c.a)(r,(function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div",[t("Project-description"),n._v(" "),t("Gradio-container")],1)}),[],!1,null,null,null);e.default=component.exports}}]);
--- _nuxt/static/1626149892/1/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FVBLab\u002F_nuxt\u002Fstatic\u002F1626149892",layout:"default",error:a,serverRendered:true,routePath:"\u002F1",config:{_app:{basePath:"\u002FVBLab\u002F",assetsPath:"\u002FVBLab\u002F_nuxt\u002F",cdnURL:a}}}}(null));
